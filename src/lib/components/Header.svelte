1dc85ee0c42be74f0d97932491c4f7faa7813b08
Emma Guillaume <emma.guillaume@mmibordeaux.com>
2023-06-20 12:36:19 +0200

    change btn header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,15 +4,15 @@
 <header
   class="px-8 w-full z-40 py-4 md:px-16 lg:px-32 bg-texture bg-cover bg-background bg-opacity-80 backdrop-blur-sm fixed flex flex-col justify-between lg:flex-row"
 >
-  <a href="#home" class="flex items-center gap-3">
+  <div class="flex items-center gap-3">
     <img class="w-12 md:w-16" src="/images/flower.svg" alt="" />
     <h1
       class="mb-2 md:mb-3 text-2xl-mob md:text-2xl text-cta font-extrabold"
       id="home"
     >
       emma guillaume .
-    </h1></a
-  >
+    </h1>
+  </div>
 
   <a href="#contact" class="mt-4 text-cta underline underline-offset-4"
     >contact</a
